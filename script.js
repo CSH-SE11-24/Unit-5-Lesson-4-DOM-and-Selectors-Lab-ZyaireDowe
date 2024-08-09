d93f964b447aae9c77157bef538f54c47425db9a
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 20:34:26 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Task 1: Console log "Script running" and confirm your script.js is linked correctly
+console.log("script running")
+
+// Task 2:
+// Console log the title of the document.
+console.log(document)
+
+// (Pink only) Task 3:
+// Console log the id of the document's body.
+const bodyID=
+console.log("document.body.id")
+
+// Task 4:
+// Select the img tag for Baby Yoda and save it in a variable.
+const babyYoda=document.querySelector("#baby-yoda-image")
+
+// Console log the variable to confirm you selected it correctly.
+
+console.log(babyYoda)
+// (Yellow skip) Console log the alt of the variable to see the image's alternative caption.
+
+// Task 5:
+// Select the p tag with the fun fact and save it in a variable.
+let p=document.querySelector("#fun-fact")
+
+// Console log the variable to confirm you selected it correctly.
+console.log(p)
+
+// (Yellow skip) Console log the textContent of the p tag to see the fun fact text
+
+
+// Task 6:
+// Add an ID to the p tag with the quote in HTML (in the div with the class quote-container).
+// Select the quote and save it in a variable.
+
+let p=("lol-quote-container")
+// Console log the variable to confirm you selected it correctly.
+console.log(p)
+
+// (Yellow skip) Task 7: 
+// Add the class "information" to the two p tags before the quote in HTML (in the container div)
+// Select the first of those p tags (selecting by class will always return the first match)
+
+
+// Console log the variable to confirm you selected it correctly.
+console.log(p)
+
+// (Pink only, extra credit for others) Look up how to select multiple elements at once and select ALL p tags with the class
+
+
+// (Pink only, extra credit for others) Console log the variable to confirm you selected it correctly 
+
+
+
+// (Extra Credit for all) Use a for loop to console log the text content of each of the selected p tags with the class
+
+
